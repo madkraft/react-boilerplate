cb4cb07b18a818ffae077375fe55d585d5ce21e9
madkraft <antonkostiuchkov@gmail.com>
2018-12-15 17:10:59 +0100

    add babel

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react'
+import { hot } from 'react-hot-loader'
+
+const Warning = React.lazy(() => import('./Warning'))
+
+class App extends Component {
+  state = {
+    count: 0
+  }
+  
+  render() {
+    return (
+      <div>
+        <h1>hello world</h1>
+        <h2>Count: {this.state.count}</h2>
+        {
+          this.state.count === 10
+          ? <React.Suspense fallback={null}><Warning /></React.Suspense>
+          : null
+        }
+      </div>
+    )
+  }
+}
+
+export default hot(module)(App)
--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
-console.log('hello');
+import React from 'react'
+import ReactDOM from 'react-dom'
+import '@babel/polyfill'
+import App from './App'
+import './styles.css'
+
+ReactDOM.render(<App />, document.getElementById('app'))
