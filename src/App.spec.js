eaea00a6d4ca70c530871cc774e9112ab31ae477
madkraft <antonkostiuchkov@gmail.com>
2018-12-15 17:40:32 +0100

    add jest

---
+++ src/App.spec.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { render } from 'react-testing-library'
+import App from './App'
+
+describe('App', () => {
+  it('Renders without error', () => {
+    render(<App/>)
+  })
+})
+
+
